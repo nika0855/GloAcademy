42dd41f53bfe49588ec49ce54a6500fce3a6ec23
nika0855 <esaul555@mail.ru>
2019-10-25 18:26:18 +0300

    Null

---
+++ lesson05/script/script.js
@@ -0,0 +1,74 @@
+'use strict';
+
+let annualPlan = {
+    money: 0,
+    income: 'фриланс', 
+    addExpenses: [],
+    deposit: true,
+    mission: 1000000,
+    period: 12
+};
+
+annualPlan.money = +prompt('Ваш месячный доход?', 100000);
+
+let arr = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+
+annualPlan.addExpenses = arr.split(',');
+
+annualPlan.deposit = confirm('Есть ли у вас депозит в банке?');
+
+let showTypeOf = function(data) {
+  console.log(data, typeof (data));
+}
+showTypeOf(annualPlan.money);
+showTypeOf(annualPlan.income);
+showTypeOf(annualPlan.deposit);
+
+let obligatoryExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+
+let howMuchIsIt1 = +prompt('Во сколько это обойдется?', 2000);
+
+let obligatoryExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+
+let howMuchIsIt2 = +prompt('Во сколько это обойдется?', 2000);
+
+let budgetMonth = annualPlan.money - (howMuchIsIt1 + howMuchIsIt2);
+
+
+annualPlan.period = annualPlan.mission / budgetMonth;
+
+
+let budgetDay = Math.floor(budgetMonth / 30);
+
+
+let getStatusIncome = function(){
+  if(budgetDay > 800) {
+    return ('Высокий уровень дохода');
+}else if(budgetDay > 300 || budgetDay < 800) {
+    return ('Средний уровень дохода');
+} else if(budgetDay > 0 || budgetDay < 300){
+    return ('Низкий уровень дохода');
+}else {
+    return ('Что то пошло не так');
+}
+}
+
+console.log('getStatusIncome: ', getStatusIncome());
+
+function getExpensesMonth (a, b) {
+  return a + b; 
+}
+let sum = getExpensesMonth(howMuchIsIt1, howMuchIsIt2);
+
+function getAccumulatedMonth(c, d){
+  return c - d;
+}
+let accumulatedMonth = getAccumulatedMonth([annualPlan.money], sum);
+console.log('Накопления за период : ', accumulatedMonth);
+
+function getTargetMonth(e, f) {
+  return e / f
+}
+
+let numMonth = getTargetMonth([annualPlan.mission], accumulatedMonth);
+console.log('Cрок достижения цели в месяцах : ', Math.floor(numMonth));
