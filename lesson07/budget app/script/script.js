85068795724a31a684649851fd3202d5cf7a976c
nika0855 <esaul555@mail.ru>
2019-10-31 20:59:35 +0300

    2 задание a-e

---
+++ lesson07/budget app/script/script.js
@@ -0,0 +1,23 @@
+let start = document.getElementById('start');
+
+let incomeAdd = document.getElementsByTagName('button');
+
+let firstBtnPlus = incomeAdd[0];
+
+let lastBtnPlus = incomeAdd[1];
+
+let depositCheck = document.querySelector('#deposit-check');
+
+let additionalIncome = document.querySelectorAll('.additional_income-item');
+
+let budgetMonth = document.querySelector('.budget_month-value'),
+budgetDay = document.querySelector('.budget_day-value'),
+expensesMonth = document.querySelector('.expenses_month-value'),
+additionalInCome = document.querySelector('.additional_income-value'),
+additionalExpenses = document.querySelector('.additional_expenses-value'),
+incomePeriod = document.querySelector('.income_period-value'),
+targetMonth = document.querySelector('.target_month-value');
+
+
+
+
