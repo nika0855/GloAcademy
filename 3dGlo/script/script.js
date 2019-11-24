df6beb753c9c9d62db02ad3344781b95a4d8a255
nika0855 <esaul555@mail.ru>
2019-11-24 16:57:58 +0300

    Timer

---
+++ 3dGlo/script/script.js
@@ -0,0 +1,44 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+'use strict';
+//Timer
+  function countTimer(deadline) {
+    let timerHourse = document.querySelector('#timer-hours'),
+        timerMinutes = document.querySelector('#timer-minutes'),
+        timerSeconds = document.querySelector('#timer-seconds');
+
+    function getTimeRemaining() {
+      let dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor(timeRemaining / 60) % 60,
+        hours = Math.floor((timeRemaining / 60 / 60) % 24),
+        days = Math.floor(timeRemaining / 60 / 60 / 24);
+        return {timeRemaining, hours, minutes, seconds};   
+    }
+
+      function updateClock() {
+        let timer = getTimeRemaining();
+
+        timerHourse.textContent = (`0${timer.hours}`).slice(-2);
+        timerMinutes.textContent = (`0${timer.minutes}`).slice(-2);
+        timerSeconds.textContent = (`0${timer.seconds}`).slice(-2);
+
+        if(timer.timeRemaining > 0) {
+          setTimeout(updateClock, 1000);
+        }else if(timer.timeRemaining < 0){
+          
+        timerHourse.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
+
+        }
+        
+      }
+      
+      updateClock();
+  }
+    
+    setInterval(countTimer, 1000, '25 nov 2019');
+});
