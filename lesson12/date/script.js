baa049d802a863ddb3a7acebcb35c46eda98d98c
nika0855 <esaul555@mail.ru>
2019-11-24 19:24:52 +0300

    2

--- lesson12/date/script.js
+++ lesson12/date/script.js
@@ -15,19 +15,19 @@ const timesDay = document.querySelector('#times-day'),
             timeRemaining = (dateStop - dateNow) / 1000,
             seconds = Math.floor(timeRemaining % 60),
             minutes = Math.floor((timeRemaining / 60) % 60),
-            hours = Math.ceil(timeRemaining / 60 / 60) % 24,
+            hours = Math.floor(timeRemaining / 60 / 60) % 24,
             day = Math.floor(timeRemaining / 60 / 60 / 24),
             timeToString = date.toLocaleTimeString('en');
 
-        if (hours <= 9) {
-            hours = `0${hours}`;
-        }
-        if (minutes <= 9) {
-            minutes = `0${minutes}`;
-        }
-        if (seconds <= 9) {
-            seconds = `0${seconds}`;
-        }
+        // if (hours <= 9) {
+        //     hours = `0${hours}`;
+        // }
+        // if (minutes <= 9) {
+        //     minutes = `0${minutes}`;
+        // }
+        // if (seconds <= 9) {
+        //     seconds = `0${seconds}`;
+        // }
 
         return { timeRemaining,hours,minutes,seconds,timeToString,day,date };
  
@@ -39,11 +39,11 @@ function output(){
     
     if (timer.hours >= 0 && timer.hours <= 6) {
         timesDay.textContent = `Доброй ночи!`;
-    } else if (timer.hours >= 6 && timer.hours < 12) {
+    } else if (timer.hours >= 6 && timer.hours <= 12) {
         timesDay.textContent = `Доброе утро!`;
     } else if (timer.hours >= 12 && timer.hours <= 18) {
         timesDay.textContent = `Добрый день!`;
-    } else if (timer.hours >= 18) {
+    } else if (timer.hours >= 18 ) {
         timesDay.textContent = `Добрый вечер!`;
     }
 
