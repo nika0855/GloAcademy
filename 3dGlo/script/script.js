0dbcb7e018a1da160e6127b7f87e2d8087fe7791
nika0855 <esaul555@mail.ru>
2019-12-07 21:26:37 +0300

    dataset

--- 3dGlo/script/script.js
+++ 3dGlo/script/script.js
@@ -265,7 +265,6 @@ let currentSlide = 0,
       startSlide();
     }
   });
-  
 
   startSlide(1500);
 
@@ -273,6 +272,31 @@ let currentSlide = 0,
 };
 
 slider();
+
+//Наша команда
+
+const replaceImg = () => {
+  const allImg = document.querySelectorAll("img[data-img]");
+  let src;
+  allImg.forEach(item => {
+      item.addEventListener("mouseover", event => {
+          src = event.target.getAttribute("src");
+          event.target.src = event.target.dataset.img;
+      });
+      item.addEventListener("mouseout", event => {
+          event.target.src = src;
+      });
+  });
+};
+replaceImg () ;
+
+
+
+
+  
+
+ 
+
 
 });
 
