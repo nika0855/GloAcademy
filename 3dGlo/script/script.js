a3c9e2ed0a34a96578cb60335c6f608d3849c838
nika0855 <esaul555@mail.ru>
2019-12-08 21:05:55 +0300

    Calc

--- 3dGlo/script/script.js
+++ 3dGlo/script/script.js
@@ -40,7 +40,7 @@ window.addEventListener('DOMContentLoaded', () => {
       updateClock();
   }
     
-    setInterval(countTimer, 1000, '5 dec 2019');
+    setInterval(countTimer, 1000, '31 dec 2019');
 
 //Menu
 const menu = document.querySelector('menu'),
@@ -308,18 +308,56 @@ const validation = (item) => {
   });
 };
 
+//калькулятор
 
+const calc = (price = 100) => {
+  const calcBlock = document.querySelector('.calc-block'),
+    calcType = document.querySelector('.calc-type'),
+    calcCount = document.querySelector('.calc-count'),
+    calcSquare = document.querySelector('.calc-square'),
+    calcDay = document.querySelector('.calc-day'),
+    totalValue = document.getElementById('total');
 
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
 
-  
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+           squareValue = +calcSquare.value;
 
- 
+      if(calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
 
+      if(calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      }else if(calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+           
+      if(typeValue && squareValue) {
+         total = price * typeValue * squareValue * countValue * dayValue;    
+      }      
+
+      totalValue.textContent = total;
+    };
+
+  calcBlock.addEventListener('change', (e) => {
+    const target = e.target;
+
+  //   if(target === calcType || target === calcSquare ||
+  //     target === calcDay || target === calcCount) {
+  //       console.log(2);
+  //     }
+  // });
+  if(target.matches('select') || target.matches('input')) {
+    countSum();
+  }
 
 });
+};
+calc(100);
 
 
-
-
-
-
+}); 
